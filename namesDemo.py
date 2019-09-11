b0f04eec2452967bf9abac695c26b0a3d5099a8f
jimskon <jamesskon@gmail.com>
2019-09-11 10:51:29 -0400

    Update namesDemo.py

--- namesDemo.py
+++ namesDemo.py
@@ -2,75 +2,79 @@
 from sortedcontainers import SortedDict
 filePath="/home/class/SoftDev/namedata/"
 
-class NameInfo:
+class nameEntry:
+  """
+  Class to store data for a single name
+  """
+  def __init__(self, line : str):
+    """
+    Takes a single line and parses it into
+    the fields
+    """
+    line=line.strip()
+    data=line.split()
+    self.name=data[0]
+    self.percent=data[1]
+    self.cumulative=data[2]
+    self.rank=data[3]
+
+class NameMap:
   """
   Class to store information about the 
   Statistics of names.
   For each name type, there is a dict
   for each name, there is a list of name %, cumulative %, rank
   """
-  def __init__(self, num : int):
-    """ 
-    create a NameInfo Structure
-    num is the number of types of names
-    """
-    self.nameData=[]
-    for i in range(num):
-      self.nameData.append([])
-  def addNames(self, nameFile, index : int):
+  def __init__(self,nameFile : str):
     """
     create a new list of names with data
     given an index within range of 0..num-1
     """
     names = open(filePath+nameFile)
-    nameDict=SortedDict()
+    self.namemap=SortedDict()
     for line in names:
-      line=line.strip()
-      data=line.split()
-      numData=data[1:]
-      numData[0] = float(numData[0])
-      numData[1] = float(numData[1])
-      numData[2] = int(numData[2])
-      nameDict[data[0]]=numData
-    self.nameData[index]=nameDict
+      nameData=nameEntry(line)
+      self.namemap[nameData.name]=nameData
     return
-  def lookup(self,name,index):
+  def lookup(self,name):
     """
-    lookup name in specified index
-    return list of [%,%cum,rank] if in list
+    lookup name in map
+    return nameEntry
     else return none
     """
-    return self.nameData[index].get(name)
+    return self.namemap.get(name)
 
-  def lookup10(self,name,index):
+  def lookup10(self,name):
     """
     lookup name in specified index
     return list of [%,%cum,rank] if in list
     else return none
     """
-    i=self.nameData[index].bisect_right(name)
+    i=self.namemap.bisect_right(name)
     print(i)
     low = max(0,i-5)
-    high = min(len(self.nameData[index]),i+5)
-    result=[]
+    high = min(len(self.namemap),i+5)
+    result = []
     for j in range(low,high):
-      result.append(self.nameData[index].peekitem(j))
+      result.append(self.namemap.peekitem(j))
     return result
 
 
 def lookup(name,data):
   print(data[name])
 
-nameInfo=NameInfo(3)
-nameInfo.addNames('dist.female.first',0)
-nameInfo.addNames('dist.male.first',1)
-nameInfo.addNames('dist.all.last',2)
+femaleMap=NameMap('dist.female.first')
+maleMap=NameMap('dist.male.first')
+lastMap=NameMap('dist.all.last')
 
-name=input("Name:").upper()
 type=int(input("1=female,2=male,3=last: "))
+name=input("Name:").upper()
 
-data=nameInfo.lookup(name,type-1)
-print(data)
-data=nameInfo.lookup10(name,type-1)
-print(data)
-
+if (type==1):
+  data=femaleMap.lookup10(name)
+elif (type==2):
+  data=maleMap.lookup10(name)
+else:
+  data=lastMap.lookup10(name)
+for namedata in data:
+  print(namedata[1].name+" : "+namedata[1].percent+"% : "+namedata[1].rank)
